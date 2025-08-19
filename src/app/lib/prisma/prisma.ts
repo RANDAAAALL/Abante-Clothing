2b00e0c70c29450455cb5696d5f51a1d5e08e3e9
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-20 03:22:28 +0800

    Added: new folder and file named prisma

---
+++ src/app/lib/prisma/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+// Extend the global object to store the Prisma instance
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+// Use existing Prisma instance in dev or create a new one
+export const prisma =
+  global.prisma || new PrismaClient({
+    log: ["query", "info", "warn", "error"], // optional: useful for debugging
+  });
+
+// Attach Prisma instance to global object in dev mode
+if (process.env.NODE_ENV === "development") {
+  global.prisma = prisma;
+}
+
+export default prisma;
