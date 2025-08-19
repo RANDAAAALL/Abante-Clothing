d5419fe3c2da331592e924b5da733f83d7f2c9cc
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-19 20:22:55 +0800

    Refactor: move metrapolis localFont object properties to a dedicated folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,10 @@
 // import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
-import localFont from "next/font/local";
+import { metrapolis } from "./utils/custom-font";
 import "./globals.css";
 
-// custom font
-const metrapolis = localFont({
-  src: [
-    { path: "../fonts/metrapolis/Metrapolis-ExtraLight.woff2", weight: "200", style: "normal" },
-    { path: "../fonts/metrapolis/Metrapolis-Thin.woff2", weight: "100", style: "normal" },
-    { path: "../fonts/metrapolis/Metrapolis-Bold.woff2", weight: "700", style: "normal" },
-    { path: "../fonts/metrapolis/Metrapolis-ExtraBold.woff2", weight: "800", style: "normal" },
-    { path: "../fonts/metrapolis/Metrapolis-Regular.woff2", weight: "400", style: "normal" },
-    { path: "../fonts/metrapolis/Metrapolis-Medium.woff2", weight: "500", style: "normal" },
-    { path: "../fonts/metrapolis/Metrapolis-Black.woff2", weight: "900", style: "normal" },
-  ],
-  variable: "--font-metrapolis",
-});
-
-
+// SEO
 export const metadata: Metadata = {
  metadataBase: new URL("https://yourproject.vercel.app"),
   title:{
