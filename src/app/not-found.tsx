6b8c00c920f1eb2f9f38e5982666b3a74391abd6
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-20 03:21:10 +0800

    Refactor: replaced anchor tag element to next/link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function NotFound(){
@@ -5,7 +6,7 @@ export default function NotFound(){
         <div className="flex h-screen flex-col items-center justify-center text-center">
         <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
         <p className="mt-2 text-gray-600"> The page you’re looking for doesn’t exist or has been moved. </p>
-        <a href="/" className="mt-4 rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"> Go Home </a>
+        <Link href="/" className="mt-4 rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"> Go Home </Link>
       </div>
     );
 }
