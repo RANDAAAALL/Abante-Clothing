a6ef0dd898aee1e9df50529dd71a633ad548e553
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-21 22:15:11 +0800

    Refactor: updated the correct path for font

--- src/lib/custom-font.ts
+++ src/lib/custom-font.ts
@@ -3,13 +3,13 @@ import localFont from "next/font/local";
 // custom font
 export const metrapolis = localFont({
     src: [
-      { path: "../../fonts/metrapolis/Metrapolis-ExtraLight.woff2", weight: "200", style: "normal" },
-      { path: "../../fonts/metrapolis/Metrapolis-Thin.woff2", weight: "100", style: "normal" },
-      { path: "../../fonts/metrapolis/Metrapolis-Bold.woff2", weight: "700", style: "normal" },
-      { path: "../../fonts/metrapolis/Metrapolis-ExtraBold.woff2", weight: "800", style: "normal" },
-      { path: "../../fonts/metrapolis/Metrapolis-Regular.woff2", weight: "400", style: "normal" },
-      { path: "../../fonts/metrapolis/Metrapolis-Medium.woff2", weight: "500", style: "normal" },
-      { path: "../../fonts/metrapolis/Metrapolis-Black.woff2", weight: "900", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-ExtraLight.woff2", weight: "200", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-Thin.woff2", weight: "100", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-Bold.woff2", weight: "700", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-ExtraBold.woff2", weight: "800", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-Regular.woff2", weight: "400", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-Medium.woff2", weight: "500", style: "normal" },
+      { path: "../fonts/metrapolis/Metrapolis-Black.woff2", weight: "900", style: "normal" },
     ],
     variable: "--font-metrapolis",
   });
