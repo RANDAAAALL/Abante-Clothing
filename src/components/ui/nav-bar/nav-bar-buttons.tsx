c61fabc1490e35452d55cc1f5a628273656233d0
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-22 19:35:26 +0800

    Refactor: made some changes on the styles

--- src/components/ui/nav-bar/nav-bar-buttons.tsx
+++ src/components/ui/nav-bar/nav-bar-buttons.tsx
@@ -5,7 +5,10 @@ export default function NavbarButtons(){
     return (
         <>
         {ButtonsPath.map((btn, i) => (
-            <Link key={i} href={btn.path} title={btn.name} className="font-metrapolis font-medium text-sl">{btn.name}</Link>
+            <Link key={i} href={btn.path} title={btn.name}
+            className={`${btn.name === "Sign Up" ? "bg-black-background rounded-lg p-2 text-white" : null}`}>
+            {btn.name}
+            </Link>
         ))}
         </>
     );
