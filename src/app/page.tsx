e2b0d0d52176b99f97b97ffb89d191bb0b6cd5c5
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-22 19:37:55 +0800

    Refactor: replace the old setup content with the NavBarContent component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import Image from "next/image";
+import NavbarContent from "@/components/ui/nav-bar/nav-bar-content";
 
 export default function Home() {
   return (
-    <div className="text-black flex flex-col justify-center items-center h-screen bg-red-background ">
-     <p className="text-black font-metrapolis font-extralight">Metrapolis Extra Light</p>
-     <p className="text-black font-metrapolis font-thin">Metrapolis Thin</p>
-     <p className="text-black font-metrapolis font-bold">Metrapolis Bold</p>
-     <p className="text-black font-metrapolis font-extrabold">Metrapolis Extra Bold</p>
-     <p className="text-black font-metrapolis font-regular">Metrapolis Regular</p>
-     <p className="font-metrapolis font-medium">Metrapolis Medium</p>
-     <p className="font-metrapolis font-black">Metrapolis Black</p>
-    <Image 
-    src="/images/svg/abante-clothing-logo.svg"
-    height={80}
-    width={80}
-    alt="Abante Clothing Logo"
-    />
+    <div className="text-black min-h-screen ">
+    <header className="font-metrapolis font-medium text-sm flex justify-evenly p-4 space-x-10 items-center">
+    <NavbarContent /> 
+    </header>
     </div>
   );
 }
