ebac074823a359f2da6dc2a355bdf83dc2b24e29
RANDAAAALL <jenniecruz0506@gmail.com>
2025-08-30 17:01:58 +0800

    Refactor: made some changes to stylings

--- src/components/ui/nav-bar-section/nav-bar-buttons.tsx
+++ src/components/ui/nav-bar-section/nav-bar-buttons.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NavbarButtons(){
     return (
         // flex ml-4 space-x-4
-        <div className="space-y-3 flex flex-col md:ml-1 md:space-y-0 md:space-x-2 md:flex md:flex-row md:items-center">
+        <div className="font-medium space-y-3 flex flex-col md:ml-1 md:space-y-0 md:space-x-2 md:flex md:flex-row md:items-center">
         {ButtonsPath.map((btn, i) => (
             <Link key={i} href={btn.path} title={btn.name}
             className={` ${btn.name === "Sign Up" ? "bg-black-background text-white dark:bg-white dark:text-black rounded-lg py-3 px-4" : null}`}>
