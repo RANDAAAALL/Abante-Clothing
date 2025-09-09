287aefa99fa007ffe902e51683c9b2c81ea4ec46
RANDAAAALL <lesterandig17@gmail.com>
2025-09-09 15:23:09 +0800

    Refactor: made some changes on stylings

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,7 +7,7 @@ export default function Register(){
     <div className="transition duration-500 ease-in-out bg-white dark:bg-black-background dark:text-white text-black w-full max-w-[1980] mx-auto">
        
         {/* nav-bar section */}
-        <header className="w-full font-regular gap-10 flex p-4 max-w-screen-lg md:justify-evenly md:items-center md:mx-auto"><NavbarContent /></header>
+        <section className="z-50 sticky top-0"><NavbarContent /></section>
 
         {/* main section */}
         <main className=" text-center w-full md:mx-auto p-4 md:p-0 md:px-6 ">
