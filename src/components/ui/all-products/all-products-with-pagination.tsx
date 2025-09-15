703ceac1a5ddc267a34a3a62727c4339cefc5878
RANDAAAALL <lesterandig17@gmail.com>
2025-09-15 22:08:32 +0800

    Added: new files

---
+++ src/components/ui/all-products/all-products-with-pagination.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { TshirtType } from "@/lib/types/t-shirt-types";
+import AllFilteredProducts from "./filtered-products";
+import PaginationSelection from "../pagination/paginated-selection";
+import usePaginationAndFilteredProducts from "@/hooks/usePaginationAndFilteredProducts";
+import Image from "next/image";
+
+export default function AllProductsWithPagination( {props}: {props: TshirtType[]} ){
+    const { itemsPerPage, currentPage, setCurrentPage, currentData } = usePaginationAndFilteredProducts({props}, 9);
+    return (
+        <>
+            {props.length === 0 ? (
+                <p className="flex justify-center items-center h-65 sm:h-100  font-medium">Search not found :/</p>
+                // <div className="flex justify-center items-center py-2">
+                //     <Image src={"/images/png/bowling.png"} height={250} width={250} alt="bowling"/>
+                // </div>
+            ) : (
+            <div className="font-bold grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <AllFilteredProducts props={currentData}/>
+            </div>
+            )}
+
+            <div className="mt-4 flex justify-center">
+                <PaginationSelection 
+                totalItems={props.length}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}/>
+            </div>
+        </>
+    );
+}
---
+++ src/data-access-layer/get-all-related-customer-product-review.ts
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prisma/prisma";
+
+
+export const getAllRelatedCustomerProductReview =  async () => {
+    const customerFeedbacks = await prisma.users_feedback.findMany({
+        select: {
+          feedback_ID: true,
+          feedback_comment: true,
+          feedback_rating: true,
+          feedback_date: true,
+          users: {
+            select: {
+              username: true,
+              user_image: true,
+            },
+          },
+        },
+    });
+
+    if(!customerFeedbacks) console.error("Customer Feedbacks not found!");
+
+    return customerFeedbacks;
+}
