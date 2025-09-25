c7fd5e3c9faa6b8966aa5b4f51f69180df5b049f
RANDAAAALL <lesterandig17@gmail.com>
2025-09-26 04:39:10 +0800

    Added: new folder for inline svg components

---
+++ src/components/icons/svg/previous-button.tsx
@@ -0,0 +1,9 @@
+
+export default function PreviousButtonSVG(){
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="-7 -1.5 24 24" fill="none" stroke="currentColor"
+        className="size-7 stroke-2 lucide lucide-chevron-left-icon lucide-chevron-left">
+        <path d="m15 18-6-6 6-6"/></svg>
+    );
+}
