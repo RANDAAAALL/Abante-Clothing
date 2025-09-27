b4d74f5539e7584f7313a9c3d7fb562f5cfc724f
RANDAAAALL <lesterandig17@gmail.com>
2025-09-28 03:12:01 +0800

    Feat: added a logout listener even for the other tabs, autoComplete attr and input id

--- src/components/ui/navbar-section/navbar-links.tsx
+++ src/components/ui/navbar-section/navbar-links.tsx
@@ -1,13 +1,16 @@
 import { LinksPath } from "@/lib/paths/links-path";
+import { useMenuBarStore } from "@/lib/store/menu-bar";
 import clsx from "clsx";
 import Link from "next/link";
 
 export default function NavbarLinks({ style }: { style: string}){
+    const { setIsOpen } = useMenuBarStore();
     return (
         <div className={clsx( style)}>
         {LinksPath.map((link, i ) => (
             <Link
             key={i} href={`${link.path}`}
+            onClick={() => setIsOpen(false)}
             title={link.name}
             className="font-medium text-sl">
             {link.name}
