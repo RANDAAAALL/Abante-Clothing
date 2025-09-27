9cdf5405edbdf6a3b3da9bf4810a1b5a8ee4f5ef
RANDAAAALL <lesterandig17@gmail.com>
2025-09-28 02:29:49 +0800

    Refactor: remove force-static and empty space

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,7 +1,5 @@
 import PrivacyPolicyContent from "@/components/ui/privacy-policy-section/privacy-policy-content";
 
-export const dynamic = 'force-static';
-
 export default function PrivacyPolicy(){
     return (
         <div className="bg-white dark:bg-black-background dark:text-white text-black min-h-screen w-full max-w-[1980] mx-auto">
