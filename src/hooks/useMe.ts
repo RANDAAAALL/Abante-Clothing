97a7ed38f734af376235215ffd22ad2802f5bd4e
RANDAAAALL <lesterandig17@gmail.com>
2025-09-28 18:40:25 +0800

    Added: new svg icon, api route, user-profile navigator and custom hook files

---
+++ src/hooks/useMe.ts
@@ -0,0 +1,48 @@
+"use client"
+import { MeURL } from "@/lib/config";
+import { useMenuBarStore } from "@/lib/store/menu-bar";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function useMe(){
+    const router = useRouter();
+    const { setIsOpen } = useMenuBarStore();
+    const queryClient = useQueryClient();
+
+    const { data, error, isError, isLoading } = useQuery({
+        queryKey: ['me'],
+        queryFn: async () =>  { 
+            console.log("Fetching user data...");
+            const res = await fetch(`${MeURL}`, {credentials: "include"})
+            if(!res.ok) throw new Error("Failed to fetch user data");
+            return res.json();
+        },
+        networkMode: "always",
+        refetchOnWindowFocus: false,  
+        refetchOnReconnect: false,   
+        retry: false,     
+        staleTime: 5 * 60 * 1000, // it will stale for 5mins longer
+    });
+
+    // listen for logout event from other tabs
+    useEffect(() => {
+        const bc = new BroadcastChannel("auth");
+        bc.onmessage = (event) => {
+        if (event.data?.type === "LOGOUT") {
+            queryClient.removeQueries({ queryKey: ["me"] });
+            router.push("/login");
+            router.refresh();
+            setIsOpen(false);
+        }
+        };
+        return () => bc.close();
+    }, [router, setIsOpen]);
+
+    return {
+        data,
+        error,
+        isError,
+        isLoading
+    }
+}
