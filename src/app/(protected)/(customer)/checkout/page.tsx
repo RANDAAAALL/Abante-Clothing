9f8d771c85207e442c4bb43556dcfe9d63dc061c
RANDAAAALL <lesterandig17@gmail.com>
2025-09-30 03:36:07 +0800

    Added: new comoponents

---
+++ src/app/(protected)/(customer)/checkout/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Checkout(){
+    return (
+        <main className="relative bg-white-card-background dark:bg-black-background dark:text-white text-black min-h-screen w-full max-w-[1980] flex items-center justify-center">
+            <span>Checkout coming soon.....</span>
+        </main>
+    );
+}
