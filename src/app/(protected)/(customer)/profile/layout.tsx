09cd1ad622a1ff539882b0b3a4a67e9ad2b66b17
RANDAAAALL <lesterandig17@gmail.com>
2025-10-06 01:53:44 +0800

    Added: new pages for profile

---
+++ src/app/(protected)/(customer)/profile/layout.tsx
@@ -0,0 +1,18 @@
+// import SidebarContent from "@/components/ui/user-profile/left-sidebar/sidebar-content";
+import SidebarContent from "@/components/ui/user-profile/left-sidebar/sidebar-content";
+
+export const experimental_ppr = true;
+
+export default function ProfileLayout({ children }: { children: React.ReactNode}){
+    return (
+    <div className="relative bg-white-card-background dark:bg-black-background dark:text-white text-black w-full max-w-[1980px] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-[300px_1fr] md:gap-10 mt-10 w-full md:max-w-3xl mx-auto p-4 md:p-0">
+        {/* sidebar */}
+        <aside><SidebarContent /></aside>
+
+        {/* right content */}
+        <main className="mt-6 md:mt-16.5">{children}</main>
+        </div>
+    </div>
+    );
+}
