5aee5f26045f979bc90367b375b1f809de0b1223
RANDAAAALL <lesterandig17@gmail.com>
2025-10-06 23:31:22 +0800

    Refactor: update config.ts

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,6 +1,6 @@
 export function getBaseUrl() {
-    if (process.env.VERCEL_URL) {
-      return `https://${process.env.VERCEL_URL}`; 
+    if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; 
     }
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
       return process.env.NEXT_PUBLIC_PROD_BASE_URL;
