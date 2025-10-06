3744488a7a589bc5c925df75b4b79ce7de9d4855
RANDAAAALL <lesterandig17@gmail.com>
2025-10-06 18:03:11 +0800

    Added: a toast client component

---
+++ src/components/ui/toast/toaster-client.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { Toaster, ToastBar } from "react-hot-toast"
+
+export default function ToasterClient() {
+  return (
+    <Toaster
+      position="top-right">
+      {(t) => (
+        <ToastBar
+          toast={t}
+          style={{
+            animation: t.visible
+              ? "toast-in 0.4s ease forwards"
+              : "toast-out 0.4s ease forwards",
+            fontSize: "15px",
+            background: "var(--card-white-background)"
+          }}
+        />
+      )}
+    </Toaster>
+  )
+}
