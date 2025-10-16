7e267919c5953fff72da7711900fdb71f63adcf0
RANDAAAALL <lesterandig17@gmail.com>
2025-10-16 18:02:43 +0800

    Add(cache): implemented cache server functions

---
+++ src/lib/cache/get-order-history-cached.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma/prisma";
+import { unstable_cache } from "next/cache";
+
+export const getOrderHistoryCached = unstable_cache(async (user_ID: number) => {
+    console.log("getOrderHistory HIT (unstable_cache)");
+
+    const data = await prisma.order_purchased.findMany({
+        where: { user_ID: user_ID},
+        select: {
+            order_purchased_number: true,
+            order_purchased_date: true,    
+            order_details: {
+                select: {
+                    order_detail_name: true,
+                    order_detail_qty: true,
+                    order_detail_size: true,
+                    order_detail_price: true,
+                }
+            }
+       }
+    });
+
+    // format the data
+    const tableData = data.flatMap(order =>
+        order.order_details.map(detail => ({
+          "ORD-NO": order.order_purchased_number ? order.order_purchased_number : "-",
+          "Product": detail.order_detail_name ? detail.order_detail_name[0].toUpperCase() + detail.order_detail_name.slice(1) : "-",
+          "Quantity": detail.order_detail_qty ? detail.order_detail_qty : 0,
+          "Price": detail.order_detail_price ? Number(detail.order_detail_price) : 0,
+          "Size": detail.order_detail_size ? detail.order_detail_size : "-",
+          "Purchased Date": order.order_purchased_date
+            ? new Date(order.order_purchased_date).toLocaleDateString()
+            : "-", 
+        }))
+      );
+      
+     return tableData;
+}, ["order-history"], { tags: ["order-history"] });
---
+++ src/lib/cache/get-user-info-cached.ts
@@ -0,0 +1,17 @@
+import { unstable_cache } from "next/cache";
+import prisma from "@/lib/prisma/prisma";
+
+export const getUserInfoCached = unstable_cache(async (userID: number) => {
+    console.log("getUserInfo HIT (unstable_cache)");
+
+    const data = await prisma.users.findUnique({
+        where: { user_ID: userID},
+        select: {
+            user_image: true,
+            username: true,
+            email: true,
+        }
+    });
+
+    return data;
+}, ["account-details"], { tags: ["account-details"] });
