1d8d075c6e6d528786d1aa92fc2d936ad0715f78
RANDAAAALL <lesterandig17@gmail.com>
2025-10-17 03:16:04 +0800

    Add(layout): new layouts

---
+++ src/components/ui/navbar-section/navbar-layout.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { ReactNode } from "react";
+import NavbarContent from "./navbar-content";
+export default function NavbarLayout({ children }: { children: ReactNode }){
+
+    return (
+        <>
+        <section className="z-50 sticky top-0"><NavbarContent/></section>
+        {children}
+        </>
+    );
+}
