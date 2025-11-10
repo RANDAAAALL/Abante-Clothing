bbd8fe8936c089541c247e48277f504949f27cdf
RANDAAAALL <lesterandig17@gmail.com>
2025-11-11 00:44:03 +0800

    Feat: added new helper function and modified some lib and page

--- src/dal/get-all-related-customer-product-review.ts
+++ src/dal/get-all-related-customer-product-review.ts
@@ -1,25 +1,39 @@
+import { filterUniqueUserFeedbacks } from "@/lib/helper/filter-unique-user-feedbacks";
 import prisma from "@/lib/prisma/prisma";
-export const getAllRelatedCustomerProductReview =  async () => {
-  try{
-    const customerFeedbacks = await prisma.users_feedback.findMany({
-          select: {
-            feedback_ID: true,
-            feedback_comment: true,
-            feedback_rating: true,
-            feedback_date: true,
-            users: {
-              select: {
-                username: true,
-                user_image: true,
-              },
-            },
+
+export const getAllRelatedCustomerProductReview = async (product_item_ID: number) => {
+  try {
+    // 1️get all feedbacks related to the clicked product
+    const relatedFeedbacks = await prisma.users_feedback.findMany({
+      where: {
+        order_details: {
+          is: {
+            product_item_ID: product_item_ID,
           },
-      });
-  
-      if(!customerFeedbacks) throw new Error("Customer Feedbacks not found!");
-      return customerFeedbacks;
-  }catch(err){
-    console.log(err);
-    return;
+        },
+      },
+      select: {
+        feedback_ID: true,
+        feedback_comment: true,
+        feedback_rating: true,
+        feedback_date: true,
+        user_ID: true,
+        users: {
+          select: {
+            username: true,
+            user_image: true,
+          },
+        },
+      },
+      orderBy: {
+        feedback_date: "desc", 
+      },
+    });
+
+    // return only unique feedbacks 1 per user
+    return filterUniqueUserFeedbacks(relatedFeedbacks);
+  } catch (err) {
+    // console.error(err);
+    return [];
   }
-}
+};
