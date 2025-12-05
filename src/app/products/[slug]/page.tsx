350ba989f202449464af1379bfde80eaa5753681
RANDAAAALL <lesterandig17@gmail.com>
2025-12-06 01:56:02 +0800

    Refactor(products/[slug]): addeed generateMetaData for seo purposes

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -10,8 +10,71 @@ import { ParamsProps } from "@/lib/types/params-types";
 import { getAllRelatedCustomerProductReview } from "@/dal/get-all-related-customer-product-review";
 import { Suspense } from "react";
 import { CustomerFeedbackProps } from "@/lib/types/customer-feedback-types";
+import type { Metadata, ResolvingMetadata } from "next";
+import { getBaseUrl } from "@/lib/helper/getBaseUrl";
 
 export const revalidate = 30;
+
+type Props = {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ color?: string | string[] }>;
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  const { slug } = await params;
+  const resolvedSearch = await searchParams;
+
+  const color = Array.isArray(resolvedSearch.color)
+    ? resolvedSearch.color[0]
+    : resolvedSearch.color;
+
+  const ProductVariants = await getSingleProduct({ slug });
+
+  if (!ProductVariants || !ProductVariants[0]) {
+    return {
+      title: "Product Not Found",
+      description: "This product is unavailable.",
+    };
+  }
+
+  const product = ProductVariants[0];
+  const mainImage = product.product_item_image ?? "/tshirt_placeholder.png";
+  console.log("Main Image URL:", mainImage);
+
+  const absoluteImageUrl = mainImage.startsWith("http")
+    ? mainImage
+    : `${getBaseUrl()}${mainImage}`;
+
+  return {
+    title: product.product_item_name![0].toUpperCase() + product.product_item_name?.slice(1)  ?? "Product",
+    description: product.product_item_design_features ?? "Check out this product!",
+    openGraph: {
+      title: product.product_item_name![0].toUpperCase()  + product.product_item_image?.slice(1) ?? "Product",
+      description: product.product_item_design_features ?? "",
+      url: `https://abante-clothing.vercel.app/products/${slug}${color ? `?color=${color}` : ""}`,
+      images: [
+        {
+          url: absoluteImageUrl,
+          width: 1200,
+          height: 630,
+          alt: product.product_item_name ?? "Product Image",
+        },
+      ],
+      type: "website",
+      siteName: "Abante Clothing",
+    },
+    twitter: {
+      title: product.product_item_name![0].toUpperCase() + product.product_item_image?.slice(1) ?? "Product",
+      card: "summary_large_image",
+      description: product.product_item_design_features ?? "",
+      images: [absoluteImageUrl],
+    },
+  };
+}
 
 export default async function Page({ params }: ParamsProps ) {
   const { slug } = await params;
